92192f95571665a112e5b1efedc2280367c48dad
chris <christofferraahauge@gmail.com>
2022-11-24 13:31:33 +0100

    Almost done

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from kivy.app import App
 
 # Grafiske komponenter
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 
@@ -11,53 +11,69 @@ from kivy.properties import StringProperty
 # Kivys interne clock til timing og framerate
 from kivy.clock import Clock
 
-# Python standard biblioteker
-import math
+import random
 
-class TimerLayout(BoxLayout):
+
+class ClickLayout(GridLayout):
     # Tekst paa knappen. Bundet via .kv. Bliver aendret i update.
     timerText = StringProperty('Set time')
-    buttonText = StringProperty('Start timer')
-    running = False        # Nedtaellingen koerer hvis sand
-    expired = False
-    startTime = 5          # Antal sekunder timeren starter på
-    timeLeft = startTime   # Antal sekunder tilbage i nedtaellingen
+    scoreText = StringProperty('0')
+    score = 0
+    clicktime = 20
+    timeLeft = clicktime
+    nextButton = 1
+    pressed = []
 
-    # Skifter running til modsatte boolske vaerdi
-    def toggle(self):
-        if self.expired and not self.running:
-            self.timeLeft = self.startTime
-            self.expired = False
-        else:
-            self.running = not self.running
-
-    # Taeller ned hvis running. Opdaterer buttonText ved hvert kald
     def update(self, dt):
-        if self.running:
-            self.timeLeft = self.timeLeft - dt
-        if self.timeLeft <= 0 and self.running:
-            self.toggle()
-            self.expired = True
+        self.timeLeft -= dt
+        self.timerText = str(round(self.timeLeft))
+        self.scoreText = str(self.score)
+        if self.timeLeft <= 0:
+            self.reset()
+            self.timeLeft = self.clicktime
 
-        self.timerText = str(math.ceil(self.timeLeft))
+    def buttonClicked(self, source):
+        print(source.text)
+        self.pressed.append(source.text)
+        self.checkCorrect()
 
-        if self.running and not self.expired:
-            self.buttonText = 'Stop'
-        elif not self.running and not self.expired:
-            self.buttonText = 'Start'
-        else:
-            self.buttonText = 'Reset'
+    def randomize(self):
+        knapliste = ['knap1', 'knap2', 'knap3', 'knap4']
+        knaptekster = ['1', '2', '3', '4']
+        # Fordel 1-4 tilfældigt på knapperne
+        random.shuffle(knaptekster)
+        for knap in knapliste:
+            self.ids[knap].text = knaptekster.pop()
 
-class BasicTimerApp(App):
+    def reset(self):
+        self.pressed = []
+
+        self.randomize()
+
+    def checkCorrect(self):
+        if len(self.pressed) > 3:
+            if self.pressed == ["1", "2", "3", "4"]:
+                print("You are not a dumb piece of shit")
+                self.score +=1
+                self.reset()
+            else:
+                print("You are a dumb piece of shit")
+                self.score -=1
+                self.reset()
+
+
+
+class ClickerApp(App):
 
     def build(self):
         # Initialiser knappen
-        layout = TimerLayout()
-
+        layout = ClickLayout()
+        layout.randomize()
         # Bed Kivy om at kalde update() 30 gange pr. sekund
-        Clock.schedule_interval(layout.update, 1.0/30.0)
+        Clock.schedule_interval(layout.update, 1.0 / 30.0)
 
         return layout
 
+
 if __name__ in ('__main__', '__android__'):
-    BasicTimerApp().run()
+    ClickerApp().run()
